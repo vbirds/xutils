c1f1c4556c1230bc0fa046be86aa39904ffdc013
don.wang <wanguandong@126.com>
2021-09-07 19:59:53 +0800

    feat: Server Authority http interface

--- xhttp.go
+++ xhttp.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -69,4 +70,35 @@ func HttpGet(url string, result interface{}) error {
 	}
 	jsoniter.Get(content, "data").ToVal(result)
 	return nil
+}
+
+// ServerOpt 服务配置信息
+type ServerOpt struct {
+	Name       string `json:"name"`
+	HttpPort   uint16 `json:"httpPort"`
+	AccessPort uint16 `json:"accessPort"`
+	RpcPort    uint16 `json:"rpcPort"`
+	Status     int    `json:"status"`
+	Address    string // 服务IP
+}
+
+// Services 服务信息
+type Server struct {
+	Local   ServerOpt `json:"local"`
+	Station ServerOpt `json:"station"`
+}
+
+func HttpApplyAuth(url, serverId string) (*Server, error) {
+	if url == "" {
+		return nil, errors.New("please set authority address firstly")
+	}
+	address := fmt.Sprintf("%s/%s", url, serverId)
+	s := &Server{}
+	if err := HttpGet(address, s); err != nil {
+		return nil, ErrorHttp(address)
+	}
+	if s.Local.Status != SERVE_StatusOk {
+		return nil, ErrDisabled
+	}
+	return s, nil
 }
