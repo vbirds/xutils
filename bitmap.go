1f743921532b1cd2a71be9423117f5a96cbf50ac
don.wang <wanguandong@126.com>
2022-03-30 15:57:56 +0800

    feat: BitMap

---
+++ bitmap.go
@@ -0,0 +1,108 @@
+package xutils
+
+import "strconv"
+
+type BitMap struct {
+	bits []uint64
+}
+
+// 最大数决定数组长度 65535/64 = 1024
+// 存储数据大小 len(bits)*64
+
+func NewBitMap(n int) *BitMap {
+	return &BitMap{
+		bits: make([]uint64, n),
+	}
+}
+
+var DefaultBitMap = NewBitMap(1024)
+
+func (b *BitMap) index(n uint) (uint, uint) {
+	return n / 64, n % 64
+}
+
+func (b *BitMap) grow(num uint) {
+	var tbits []uint64
+	if num+1 < uint(len(b.bits))+1024 {
+		tbits = make([]uint64, len(b.bits)+1024)
+	} else {
+		tbits = make([]uint64, num+1)
+	}
+	copy(tbits, b.bits)
+	b.bits = tbits
+}
+
+func (b *BitMap) Set(n int) {
+	index, offset := b.index(uint(n))
+	if index+1 > uint(len(b.bits)) {
+		b.grow(index)
+	}
+	b.bits[index] ^= 1 << (offset) // 每一位代表一个数
+}
+
+func (b *BitMap) Include(n int) bool {
+	index, offset := b.index(uint(n))
+	if index+1 > uint(len(b.bits)) {
+		return false
+	}
+	if b.bits[index]&(1<<offset) > 0 {
+		return true
+	}
+	return false
+}
+
+func (b *BitMap) Del(n int) {
+	index, offset := b.index(uint(n))
+	if index+1 > uint(len(b.bits)) {
+		return
+	}
+	b.bits[index] &^= 1 << offset
+	if b.bits[len(b.bits)-1] != 0 {
+		return
+	}
+	// 缩容
+	i := len(b.bits) - 1
+	for ; i >= 0; i-- {
+		// 计算当前数组不为0的位置
+		if b.bits[i] == 0 && i != len(b.bits)-1024 {
+			continue
+		}
+		break
+	}
+	if i < len(b.bits)/2 || i == len(b.bits)-1024 {
+		// 小于总组数一半或超过1023个,进行缩容
+		b.bits = b.bits[:i+1]
+	}
+}
+
+func (b *BitMap) Clear() {
+	b.bits = make([]uint64, 0)
+}
+
+func (b *BitMap) All() []int {
+	var rs []int
+	for j := 0; j < len(b.bits); j++ {
+		for i := 0; i < 64; i++ {
+			if b.bits[j]&(1<<i) > 0 {
+				rs = append(rs, j*64+i)
+			}
+		}
+	}
+	return rs
+}
+
+func (b *BitMap) String() string {
+	var s string
+	for j := 0; j < len(b.bits); j++ {
+		for i := 0; i < 64; i++ {
+			if b.bits[j]&(1<<i) > 0 {
+				s += strconv.Itoa(j*64 + i)
+				s += ","
+			}
+		}
+	}
+	if s != "" {
+		return s[:len(s)-1]
+	}
+	return s
+}
--- utils_test.go
+++ utils_test.go
@@ -32,3 +32,14 @@ func TestXNet(t *testing.T) {
 	s.Handler = handler
 	s.ListenAndServe()
 }
+
+func TestBitmap(t *testing.T) {
+	bmp := DefaultBitMap
+	for i := 0; i < 500; i++ {
+		bmp.Set(1000000 + i)
+	}
+	fmt.Println(bmp.Include(63))
+	fmt.Println(bmp.Include(67))
+	fmt.Println(bmp.All())
+	fmt.Println(len(bmp.bits))
+}
